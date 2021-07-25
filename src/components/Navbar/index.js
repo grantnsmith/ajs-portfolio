22e917d23f104c273f02642d0a17016b07853c0c
Grant Smith <grantnsmith@gmail.com>
2021-07-25 15:36:24 -0700

    Update Navbar.

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,34 +1,49 @@
 import React from "react";
 import "./style.css";
 
+import { NavLink } from "react-router-dom";
+
 function Navbar() {
   return (
-    <nav>
-      <ul className="menu">
-        <li className="logo">
-          <a href="/">AJS Interview Coaching</a>
-        </li>
-        <li className="item">
-          <a href="/" className="button">
-            Home
-          </a>
-        </li>
-        <li className="item">
-          <a href="/services" className="button">
-            Services
-          </a>
-        </li>
-        <li className="item">
-          <a href="/contact" className="button">
-            Contact
-          </a>
-        </li>
-        <li class="toggle">
-          <a href="#">
-            <i className="fas fa-bars"></i>
-          </a>
-        </li>
-      </ul>
+    <nav className="navbar navbar-expand-lg navbar-light">
+      <button
+        className="navbar-toggler ml-auto pull-right"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item gns-nav-align">
+            <NavLink exact to="/" activeClassName="active" className="nav-link">
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item gns-nav-align">
+            <NavLink
+              to="/services"
+              activeClassName="active"
+              className="nav-link"
+            >
+              Services
+            </NavLink>
+          </li>
+          <li className="nav-item gns-nav-align">
+            <NavLink
+              to="/contact"
+              activeClassName="active"
+              className="nav-link"
+            >
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
